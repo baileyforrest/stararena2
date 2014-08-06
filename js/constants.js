53bfe11e6b07bdbd3ec61e2b22aad10ff675585c
Bailey Forrest <baileycforrest@gmail.com>
2014-08-05 23:52:14 -0700

    added constants file

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+/**
+ * constants.js
+ */
+
+// TODO: update these to better colors
+var SHIELD_COLOR = vec4.fromValues(0.0, 0.0, 1.0, 1.0);
+var ARMOR_COLOR = vec4.fromValues(0.0, 1.0, 0.0, 1.0);
+var HULL_COLOR = vec4.fromValues(1.0, 0.0, 1.0, 1.0);
